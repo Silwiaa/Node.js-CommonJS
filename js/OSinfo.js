700f4c57d2e0bd52b49d4f1bddaf32a7565e290b
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-05-26 13:11:24 +0200

    create OSinfo modul

---
+++ js/OSinfo.js
@@ -0,0 +1,20 @@
+function getOSinfo() {
+    var type = os.type();
+    
+    if(type === 'Darwin') {
+        type = 'OSX';
+    } else if(type === 'Windows_NT') {
+        type = 'Windows';
+    }
+    var release = os.release(),
+        cpu = os.cpus()[0].model,
+        uptime = os.uptime(),
+        userInfo = os.userInfo();
+    
+    console.log('System:', type);
+    console.log('Release:', release);
+    console.log('CPU model:', cpu);
+    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
+    console.log('User name:', userInfo.username);
+    console.log('Home dir:', userInfo.homedir);
+}
