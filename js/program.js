13b268ed74f17f63aa43d0955f40f76a5eabfe2a
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-05-26 13:05:04 +0200

    add getOSfunction

--- js/program.js
+++ js/program.js
@@ -39,6 +39,7 @@ process.stdin.on('readable', function() {
                 //USERINFO
                 var userInfo = os.userInfo();
                 console.log('User name:', userInfo.username);
+                console.log('Home dir:', userInfo.homedir);
                 
                 break;
             default:
@@ -47,3 +48,24 @@ process.stdin.on('readable', function() {
     }
     console.log(process.versions); 
 });
+
+function getOSinfo() {
+    var type = os.type();
+    
+    if(type === 'Darwin') {
+        type = 'OSX';
+    } else if(type === 'Windows_NT') {
+        type = 'Windows';
+    }
+    var release = os.release(),
+        cpu = os.cpus()[0].model,
+        uptime = os.uptime(),
+        userInfo = os.userInfo();
+    
+    console.log('System:', type);
+    console.log('Release:', release);
+    console.log('CPU model:', cpu);
+    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
+    console.log('User name:', userInfo.username);
+    console.log('Home dir:', userInfo.homedir);
+}
